fb24a26713c298c3c551fce2f1578d6754aad904
Martin <mato.stefko@gmail.com>
2020-07-08 10:21:26 +0200

    Modificator  of .sh fles  is removed

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
-from pathlib import Path
+# from pathlib import Path
 
 
-path: Path
-for path in Path(".").glob("**/*.sh"):
-    data = path.read_bytes()
-    lf_data = data.replace(b"\r\n", b"\n")
-    path.write_bytes(lf_data)
+# path: Path
+# for path in Path(".").glob("**/*.sh"):
+#     data = path.read_bytes()
+#     # lf_data = data.replace(b"\r\n", b"\n")
+#     path.write_bytes(lf_data)
